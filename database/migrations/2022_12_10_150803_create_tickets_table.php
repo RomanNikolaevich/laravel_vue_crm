6ce83b35fefde679e2cc0250ea3c2e62fda11bcd
RomanNikolaevich <roman.nikolaevich@gmail.com>
2022-12-10 17:20:17 +0200

    v3: Настройка соединения с БД, Миграции

--- database/migrations/2022_12_10_150803_create_tickets_table.php
+++ database/migrations/2022_12_10_150803_create_tickets_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->unsignedTinyInteger('status')->default(0);
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('tickets');
     }
 };
